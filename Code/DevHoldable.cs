fc89cc3d8293d650048c50726edd3e81930b3378
elliot! <benjikrolfanpageuwu@gmail.com>
2026-02-23 17:23:23 +0000

    Updated to be compatable with the latest game updates

--- Code/DevHoldable.cs
+++ Code/DevHoldable.cs
@@ -1,46 +1,81 @@
-﻿using GorillaLocomotion;
+
+﻿using System;
+using GorillaLocomotion;
 using UnityEngine;
-using UnityEngine.XR;
 
 namespace DevHoldableEngine
 {
     public class DevHoldable : HoldableObject
     {
-        public bool
-            InHand = false,
+        public bool InHand = false,
             InLeftHand = false,
             PickUp = true,
             DidSwap = false,
             SwappedLeft = true;
 
-        public float
-            GrabDistance = 0.15f,
+        public float Distance = 0.13f,
             ThrowForce = 1.75f;
+
+        public Action OnPickUp;
+        public Action OnSwapHands;
+        public Action OnPutDown;
 
         public virtual void OnGrab(bool isLeft)
         {
-
+            OnPickUp?.Invoke();
         }
 
         public virtual void OnDrop(bool isLeft)
         {
+            if (isLeft)
+            {
+                InLeftHand = true;
+                InHand = false;
+                transform.SetParent(null);
 
+                EquipmentInteractor.instance.leftHandHeldEquipment = null;
+            }
+            else
+            {
+                InLeftHand = false;
+                InHand = false;
+                transform.SetParent(null);
+
+                EquipmentInteractor.instance.rightHandHeldEquipment = null;
+            }
+            OnPutDown?.Invoke();
         }
 
         public void Update()
         {
-            InputDevices.GetDeviceAtXRNode(XRNode.LeftHand).TryGetFeatureValue(CommonUsages.grip, out float left);
-            bool leftGrip = left >= 0.5f;
+            var leftGrip = ControllerInputPoller.instance.leftControllerGripFloat > 0.6f;
+            var rightGrip = ControllerInputPoller.instance.rightControllerGripFloat > 0.6f;
 
-            InputDevices.GetDeviceAtXRNode(XRNode.RightHand).TryGetFeatureValue(CommonUsages.grip, out float right);
-            bool rightGrip = right >= 0.5f;
+            DidSwap = (!DidSwap || (!SwappedLeft ? leftGrip : rightGrip)) && DidSwap;
 
-            var Distance = GrabDistance * Player.Instance.scale;
-            if (DidSwap && (!SwappedLeft ? !leftGrip : !rightGrip))
-                DidSwap = false;
-
-            bool pickLeft = PickUp && leftGrip && Vector3.Distance(Player.Instance.leftControllerTransform.position, transform.position) < Distance && !InHand && EquipmentInteractor.instance.leftHandHeldEquipment == null && !DidSwap;
-            bool swapLeft = InHand && leftGrip && rightGrip && !DidSwap && (Vector3.Distance(Player.Instance.leftControllerTransform.position, transform.position) < Distance) && !SwappedLeft && EquipmentInteractor.instance.leftHandHeldEquipment == null;
+            var pickLeft =
+                PickUp
+                && leftGrip
+                && Vector3.Distance(
+                    GTPlayer.Instance.LeftHand.controllerTransform.position,
+                    transform.position
+                )
+                    < Distance * GTPlayer.Instance.scale
+                && !InHand
+                && EquipmentInteractor.instance.leftHandHeldEquipment == null
+                && !DidSwap;
+            var swapLeft =
+                InHand
+                && leftGrip
+                && rightGrip
+                && !DidSwap
+                && Vector3.Distance(
+                    GTPlayer.Instance.LeftHand.controllerTransform.position,
+                    transform.position
+                )
+                    < Distance * GTPlayer.Instance.scale
+                && !SwappedLeft
+                && EquipmentInteractor.instance.leftHandHeldEquipment == null;
             if (pickLeft || swapLeft)
             {
                 DidSwap = swapLeft;
@@ -49,24 +84,42 @@ namespace DevHoldableEngine
                 InHand = true;
 
                 transform.SetParent(GorillaTagger.Instance.offlineVRRig.leftHandTransform.parent);
-                GorillaTagger.Instance.StartVibration(true, 0.1f, 0.05f);
+                GorillaTagger.Instance.StartVibration(true, 0.07f, 0.07f);
                 EquipmentInteractor.instance.leftHandHeldEquipment = this;
-                if (DidSwap) EquipmentInteractor.instance.rightHandHeldEquipment = null;
+                if (DidSwap)
+                    EquipmentInteractor.instance.rightHandHeldEquipment = null;
 
                 OnGrab(true);
             }
-            else if (!leftGrip && InHand && InLeftHand)
+            else if (!leftGrip && InHand && InLeftHand || !PickUp && InHand)
             {
-                InLeftHand = true;
-                InHand = false;
-                transform.SetParent(null);
-
-                EquipmentInteractor.instance.leftHandHeldEquipment = null;
                 OnDrop(true);
+                return;
             }
 
-            bool pickRight = PickUp && rightGrip && Vector3.Distance(Player.Instance.rightControllerTransform.position, transform.position) < Distance && !InHand && EquipmentInteractor.instance.rightHandHeldEquipment == null && !DidSwap;
-            bool swapRight = InHand && leftGrip && rightGrip && !DidSwap && (Vector3.Distance(Player.Instance.rightControllerTransform.position, transform.position) < Distance) && SwappedLeft && EquipmentInteractor.instance.rightHandHeldEquipment == null;
+            bool pickRight =
+                PickUp
+                && rightGrip
+                && Vector3.Distance(
+                    GTPlayer.Instance.RightHand.controllerTransform.position,
+                    transform.position
+                )
+                    < Distance * GTPlayer.Instance.scale
+                && !InHand
+                && EquipmentInteractor.instance.rightHandHeldEquipment == null
+                && !DidSwap;
+            bool swapRight =
+                InHand
+                && leftGrip
+                && rightGrip
+                && !DidSwap
+                && Vector3.Distance(
+                    GTPlayer.Instance.RightHand.controllerTransform.position,
+                    transform.position
+                )
+                    < Distance * GTPlayer.Instance.scale
+                && SwappedLeft
+                && EquipmentInteractor.instance.rightHandHeldEquipment == null;
             if (pickRight || swapRight)
             {
                 DidSwap = swapRight;
@@ -76,21 +129,33 @@ namespace DevHoldableEngine
                 InHand = true;
                 transform.SetParent(GorillaTagger.Instance.offlineVRRig.rightHandTransform.parent);
 
-                GorillaTagger.Instance.StartVibration(false, 0.1f, 0.05f);
+                GorillaTagger.Instance.StartVibration(false, 0.07f, 0.07f);
                 EquipmentInteractor.instance.rightHandHeldEquipment = this;
-                if (DidSwap) EquipmentInteractor.instance.leftHandHeldEquipment = null;
+                if (DidSwap)
+                    EquipmentInteractor.instance.leftHandHeldEquipment = null;
 
                 OnGrab(false);
             }
-            else if (!rightGrip && InHand && !InLeftHand)
+            else if (!rightGrip && InHand && !InLeftHand || !PickUp && InHand)
             {
-                InLeftHand = false;
-                InHand = false;
-                transform.SetParent(null);
-
-                EquipmentInteractor.instance.rightHandHeldEquipment = null;
                 OnDrop(false);
+                return;
             }
+        }
+
+        public override void OnHover(InteractionPoint pointHovered, GameObject hoveringHand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnGrab(InteractionPoint pointGrabbed, GameObject grabbingHand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DropItemCleanup()
+        {
+            throw new NotImplementedException();
         }
     }
 }
